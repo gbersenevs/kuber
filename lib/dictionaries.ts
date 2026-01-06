f28e85ee2e3a468221a3016631a0d7ea049cf5bc
gbersenevs <glebusheksss@gmail.com>
2026-01-06 22:31:07 +0200

    3

---
+++ lib/dictionaries.ts
@@ -0,0 +1,10 @@
+import 'server-only';
+
+const dictionaries = {
+  en: () => import('./dictionaries/en.json').then((module) => module.default),
+  lv: () => import('./dictionaries/lv.json').then((module) => module.default),
+};
+
+export const getDictionary = async (locale: 'en' | 'lv') =>
+  dictionaries[locale]();
+
